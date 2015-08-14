c0ed2f0f85a8c4131e88789877ef2c1301019bff
Vojta Bulant <vojtech@bulant.cz>
2015-08-14 10:31:47 +0200

    break instead of continue, reformat prototype

--- www/js/dependent-checkboxes.js
+++ www/js/dependent-checkboxes.js
@@ -8,38 +8,35 @@ var DependentCheckboxes = function(container) {
     ;
 };
 
-DependentCheckboxes.prototype = {
+DependentCheckboxes.prototype.handleCheckAllChange = function(e) {
+    $(':checkbox').prop('checked', e.currentTarget.checked);
+};
 
-    handleCheckAllChange: function(e) {
-        $(':checkbox').prop('checked', e.currentTarget.checked);
-    },
+DependentCheckboxes.prototype.handleCheckGroupChange = function(e) {
+    var checkbox = e.currentTarget;
+    $(':checkbox[data-category="'+ checkbox.getAttribute('data-group') +'"]').prop('checked', checkbox.checked);
+};
 
-    handleCheckGroupChange: function(e) {
-        var checkbox = e.currentTarget;
-        $(':checkbox[data-category="'+ checkbox.getAttribute('data-group') +'"]').prop('checked', checkbox.checked);
-    },
+DependentCheckboxes.prototype.handleCheckboxChange = function() {
+    'use strict';
 
-    handleCheckboxChange: function(e) {
-        'use strict';
+    var groups = this.container.find('.column');
+    var allGroupsChecked = true;
 
-        var groups = this.container.find('.column');
-        var allGroupsChecked = true;
+    for (let i = 0; i < groups.length; i++) {
+        let group = $(groups[i]);
+        let checkboxes = group.find('ul :checkbox');
+        let allChecked = true;
 
-        for (let i = 0; i < groups.length; i++) {
-            let group = $(groups[i]);
-            let checkboxes = group.find('ul :checkbox');
-            let allChecked = true;
-
-            for (let i = 0; i < checkboxes.length; i++) {
-                if (!checkboxes[i].checked) {
-                    allGroupsChecked = allChecked = false;
-                    continue;
-                }
+        for (let i = 0; i < checkboxes.length; i++) {
+            if (!checkboxes[i].checked) {
+                allGroupsChecked = allChecked = false;
+                break;
             }
-
-            group.find(':checkbox[data-group]').prop('checked', allChecked);
         }
 
-        this.container.find(':checkbox.check-all').prop('checked', allGroupsChecked);
+        group.find(':checkbox[data-group]').prop('checked', allChecked);
     }
+
+    this.container.find(':checkbox.check-all').prop('checked', allGroupsChecked);
 };

9751f8be5a459a4cd098ad795b53f1533ac63148
Jakub Zavrel <jakubzavrel@seznam.cz>
2015-09-17 15:51:12 +0200

    ecmascript 2015 version

---
+++ www/app/dependent-checkboxes-es2015.js
@@ -0,0 +1,101 @@
+class DependentCheckboxes {
+    constructor(container) {
+        this.container = container;
+        this.checkboxes = this.container.getElementsByTagName('input');//find('input[type="checkbox"]');
+        this.checkgroup = this.container.querySelectorAll('[data-group]');
+        this.checkmaster = this.container.getElementsByClassName('check-all');//return array of elements I need one object
+        this.checkmaster = this.checkmaster[0];
+
+        //this.checkmaster.addEventListener('change', this.CheckAll.bind(this)); //that sucks http://stackoverflow.com/questions/15485030/why-am-i-getting-typeerror-obj-addeventlistener-is-not-a-function
+        this.container.addEventListener('change', this.handleWhateverHappens.bind(this));
+    }
+
+    handleWhateverHappens(e) {
+        var target = e.target; //event.target identifies the element on which the event occurred *** currentTarget refers to the element the event handler has been attached
+
+        if (target.classList.contains('check-all')) {
+            //console.log("master of all checkboxes");
+            this.CheckAll();
+        } else if (target.getAttribute('data-group')) {
+            //console.log("master of group");
+            this.CheckGroup(e);
+        } else {
+            //console.log("poor boring checkbox");
+            this.VerifiedGroup(e);
+        }
+
+    }
+
+    CheckAll(e) {
+        var statusOfMaster = this.checkmaster.checked;
+        var checkboxesLength = this.checkboxes.length;
+
+        for (var i = 0 ; i < checkboxesLength; i++) {
+            if (statusOfMaster) {
+                this.checkboxes[i].checked = true;
+            } else {
+                this.checkboxes[i].checked = false;
+            }
+        };
+    };
+
+    CheckGroup(e) {
+        //alert("yeeehaaa");
+        var target = e.target;
+        var statusOfGroup = target.checked;
+        var group = target.getAttribute('data-group');
+        var checkboxesInGroup = this.container.querySelectorAll('input[data-category="' + group + '"]');
+        var checkboxesInGroupLength = checkboxesInGroup.length;
+
+        for (var i = 0 ; i < checkboxesInGroupLength; i++) {
+            if (statusOfGroup) {
+                checkboxesInGroup[i].checked = true;
+            } else {
+                checkboxesInGroup[i].checked = false;
+            }
+        }
+
+        this.VerifiedMaster();
+    };
+
+    VerifiedGroup(e) {
+        var group = e.target.getAttribute('data-category');
+        var allCheckboxiesInGroup = this.container.querySelectorAll('input[data-category="' + group + '"]');
+        var counter = 0;
+
+        for (var x = 0; x < allCheckboxiesInGroup.length; x++) {
+            if (allCheckboxiesInGroup[x].checked) {
+                counter++;
+            }
+        }
+
+        if (counter === allCheckboxiesInGroup.length) {
+            var help = this.container.querySelectorAll('input[data-group="' + group + '"]');
+            help[0].checked = true;
+        } else {
+            var help = this.container.querySelectorAll('input[data-group="' + group + '"]');
+            help[0].checked = false;
+        }
+
+        this.VerifiedMaster();
+    };
+
+    VerifiedMaster() {
+        var counter = 0;
+        var groupLength = this.checkgroup.length;
+
+        for (var x = 0; x < groupLength; x++) {
+            if (this.checkgroup[x].checked) {
+                counter++;
+            }
+        }
+
+        if (counter === groupLength) {
+            this.checkmaster.checked = true;
+        } else {
+            this.checkmaster.checked = false;
+        }
+    };
+}
+
+window.DependentCheckboxes = DependentCheckboxes;
